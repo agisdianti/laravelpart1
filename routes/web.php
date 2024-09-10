c69c36b76f4bf98e68a4286d3621c155152104e0
agis <agissdianti@gmsil.com>
2024-09-10 14:30:40 +0700

    menambahkan eloqunt

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,17 @@
 
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\StoreController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [HomeController::class, "index"]);
+//Route::get('/home', [HomeController::class, "index"]);
 
-Route::get('/book', [BookController::class, 'index']);
+//Route::get('/book', [BookController::class, 'index']);
+
+Route::get("/create", [StoreController::class,"store"]);
+
+Route::get('/show', [StoreController::class, 'show']);
